145a141750f923acc64710e5b16d4be5f7d57904
psypher <akashrajput.savvy@gmail.com>
2024-07-10 20:12:19 +0530

    base layer ops pushed

---
+++ layerops.py
@@ -0,0 +1,39 @@
+import numpy as np
+from core import ParamOperation
+
+"""
+Dense Layer
+Class of Parametric Operations
+"""
+
+
+class WeightMul(ParamOperation):
+
+    def __init__(self, W: np.ndarray):
+        super().__init__(W)
+
+    def _output(self, inference: bool = False) -> np.ndarray:
+        return np.matmul(self.input_, self.param)
+
+    def _input_grad(self, output_grad: np.ndarray) -> np.ndarray:
+        return np.matmul(output_grad, self.param.T)
+
+    def _param_grad(self, output_grad: np.ndarray) -> np.ndarray:
+        return np.matmul(self.input_.T, output_grad)
+
+
+class BiasAdd(ParamOperation):
+
+    def __init__(self, B: np.ndarray):
+        super().__init__(B)
+
+    def _output(self, inference: bool = False) -> np.ndarray:
+        return np.add(self.input_, self.param)
+
+    def _input_grad(self, output_grad: np.ndarray) -> np.ndarray:
+        return np.ones_like(self.input_) * output_grad
+
+    def _param_grad(self, output_grad: np.ndarray) -> np.ndarray:
+        param_grad = np.ones_like(self.param)
+        output_grad_reshaped = np.sum(output_grad, axis=0).reshape(1, -1)
+        return param_grad * output_grad_reshaped
